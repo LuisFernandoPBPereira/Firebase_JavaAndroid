89fc19ae29bbfe749bfd2ab1cd61a8c702df45e0
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2022-06-01 23:50:48 -0300

    adding the project structure
    The project structure is here with Firebase included, and a insert realized

---
+++ app/src/main/java/com/example/projetofirebase/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.projetofirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference dados = FirebaseDatabase.getInstance().getReference();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dados.child("usuario").child("001").child("Nome").setValue("Luis Fernando");
+    }
+}
